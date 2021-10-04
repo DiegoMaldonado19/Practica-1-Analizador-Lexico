766c6e49c66731d69e00e34ca62ad22e8d355f36
Diego Maldonado <dj_maldonado19@hotmail.es>
2021-10-04 00:48:50 -0600

    Agregando Inicio de Practica

---
+++ src/main/java/modelo/Lexema.java
@@ -0,0 +1,74 @@
+
+package modelo;
+
+/**
+ *
+ * @author ACER
+ */
+public class Lexema {
+    /**
+     * Clase Lexema para generar objetos de tipo lexema, de esta manera guardarmos su contenido y tipo
+     */
+    
+    /**
+     * Arreglo de chars para almacenar cada elemento que conforma el lexema
+     */
+    private char[] lexema;
+    /**
+     * Objeto de tipo enum para saber que tipo de lexema tenemos
+     */
+    private TipoLexema tipoLexema;
+
+    /**
+     * Constructor por defecto de la clase lexema
+     * @param lexema conjunto de chars que conforman el lexema
+     * @param tipoLexema  tipo de lexema
+     */
+    public Lexema(char[] lexema, TipoLexema tipoLexema) {
+        this.lexema = lexema;
+        this.tipoLexema = tipoLexema;
+    }
+
+    /**
+     * Constructor para instanciar un objeto solo con su conjunto de chars
+     * @param lexema conjunto de caracteres que conforman el lexema
+     */
+    public Lexema(char[] lexema) {
+        this.lexema = lexema;
+    }
+
+    /**
+     * Getter para el arreglo de caracteres
+     * @return 
+     */
+    public char[] getLexema() {
+        return lexema;
+    }
+
+    /**
+     * Setter para definir un arreglo de caracteres
+     * @param lexema 
+     */
+    public void setLexema(char[] lexema) {
+        this.lexema = lexema;
+    }
+
+    /**
+     * Getter para obtener el tipo de lexema
+     * @return 
+     */
+    public TipoLexema getTipoLexema() {
+        return tipoLexema;
+    }
+
+    /**
+     * Setter para setear el tipo de lexema
+     * @param tipoLexema 
+     */
+    public void setTipoLexema(TipoLexema tipoLexema) {
+        this.tipoLexema = tipoLexema;
+    }
+    
+    
+    
+}
---
+++ src/main/java/modelo/TipoLexema.java
@@ -0,0 +1,13 @@
+
+package modelo;
+
+/**
+ *
+ * @author ACER
+ */
+public enum TipoLexema {
+    /**
+     * Conjunto de enumeraciones para describir el tipo de lexema que tenemos
+     */
+    IDENTIFICADOR, NUMERO, DECIMAL, PUNTUACION, OPERADOR, AGRUPACION
+}
